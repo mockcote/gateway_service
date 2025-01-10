b5dc5a2eaad547b777762b8bea01ce36f7e7a50c
binaryarc <jjin4363@gmail.com>
2025-01-10 23:54:58 +0900

    fix: gateway logging 추가

--- gateway-service/src/main/java/com/example/gatewayservice/filter/LoggingFilter.java
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/LoggingFilter.java
@@ -4,10 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class LoggingFilter implements GlobalFilter {
@@ -15,7 +21,38 @@ public class LoggingFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("Path of the request received -> {}", exchange.getRequest().getPath());
-        return chain.filter(exchange);
+        // 요청 정보 로깅
+        log.info("Request Path: {}", exchange.getRequest().getPath());
+        log.info("HTTP Method: {}", exchange.getRequest().getMethod());
+        log.info("Query Params: {}", exchange.getRequest().getQueryParams());
+        log.info("Headers: {}", exchange.getRequest().getHeaders());
+
+        // 요청 Body 처리
+        return DataBufferUtils.join(exchange.getRequest().getBody())
+                .flatMap(dataBuffer -> {
+                    String body = StandardCharsets.UTF_8.decode(dataBuffer.asByteBuffer()).toString();
+                    log.info("Request Body: {}", body);
+
+                    // DataBuffer 복제
+                    DataBuffer cachedBuffer = exchange.getResponse().bufferFactory().wrap(body.getBytes(StandardCharsets.UTF_8));
+
+                    // 기존 DataBuffer 해제
+                    DataBufferUtils.release(dataBuffer);
+
+                    // 새로운 ServerHttpRequest 생성
+                    ServerHttpRequest mutatedRequest = new ServerHttpRequestDecorator(exchange.getRequest()) {
+                        @Override
+                        public Flux<DataBuffer> getBody() {
+                            return Flux.just(cachedBuffer);
+                        }
+                    };
+
+                    // 변환된 ServerWebExchange 생성
+                    ServerWebExchange mutatedExchange = exchange.mutate()
+                            .request(mutatedRequest)
+                            .build();
+
+                    return chain.filter(mutatedExchange);
+                });
     }
 }
